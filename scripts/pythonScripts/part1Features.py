cd0a60a129e9cf6b9800a4906b24344d34714768
Kostas Alexandridis <ktalexan@outlook.com>
2025-05-09 04:09:15 -0700

    Add initial script for OCSWITRS R Data Processing project
    - Created part1Features.py with project metadata and library imports
    - Established preliminary structure for future development
    - Included necessary libraries such as arcpy and arcgis for geospatial processing

---
+++ scripts/pythonScripts/part1Features.py
@@ -0,0 +1,34 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# OCSWITRS R Data Processing #
+# PROJECT FUNCTIONS ####
+# v 2.1, May 2025
+# Dr. Kostas Alexandridis, GISP
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# region 1. Preliminaries
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# region 1.1. Referencing Libraries and Initialization
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Import Python libraries
+import os, json, pytz, math, arcpy, arcgis
+from datetime import date, time, datetime, timedelta, tzinfo, timezone
+from arcpy import metadata as md
+
+# important as it "enhances" Pandas by importing these classes (from ArcGIS API for Python)
+from arcgis.features import GeoAccessor, GeoSeriesAccessor
+
+# endregion
+
+
+# region 1.2. Project and Workspace Variables
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# endregion
+
+# endregion
+
